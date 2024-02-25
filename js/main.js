241a02f7b5c0d1f1f0891058d6a5cbecaca6ce81
Jesús Ramon Martinez Cervantes <martinezcjr1@gmail.com>
2024-02-25 14:13:59 -0800

    'first commit'

---
+++ js/main.js
@@ -0,0 +1,44 @@
+
+
+
+/*function checkMunicipios() {
+    const municipioTitles = document.querySelectorAll('.municipio__item__title');
+    const munDescription = document.querySelectorAll('.municipio__description');
+    municipioTitles.forEach((element, index) => {
+        element.addEventListener('click', event => {
+            event.stopPropagation();
+            munDescription.forEach((content, ind) => {
+                if (index!==ind) {
+                    content.classList.remove('active');
+                }
+            });
+            municipioTitles.forEach((content, ind) => {
+                if (index!==ind) {
+                    content.classList.remove('active');
+                }
+            });
+            munDescription[index].classList.toggle('active');
+            municipioTitles[index].classList.toggle('active');
+        });
+    });
+};
+checkMunicipios();*/
+
+function checkMunicipios() {
+    const municipioTitles = document.querySelectorAll('.municipio__item__title');
+    const munDescription = document.querySelectorAll('.municipio__description');
+    municipioTitles.forEach((element, index) => {
+        element.addEventListener('click', event => {
+            event.stopPropagation();
+            munDescription.forEach((content, ind) => {
+                if (index!==ind) {
+                    content.classList.remove('active');
+                    municipioTitles[ind].classList.remove('active'); // Remover la clase active de los títulos no seleccionados
+                }
+            });
+            munDescription[index].classList.toggle('active'); //Alterna la clase active de la descripción seleccionada 
+            municipioTitles[index].classList.toggle('active'); // Alternar la clase active del título seleccionado
+        });
+    });
+};
+checkMunicipios();
